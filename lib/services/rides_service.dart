f12a5f530eb6f2c35f93baa403429b8d4f3f41be
LK-Hour <l.kimhour.609@gmail.com>
2026-02-05 11:26:46 +0700

    BLA-001-Rides Service

--- lib/services/rides_service.dart
+++ lib/services/rides_service.dart
@@ -1,4 +1,5 @@
 import '../data/dummy_data.dart';
+import '../model/ride/locations.dart';
 import '../model/ride/ride.dart';
 
 ////
@@ -6,7 +7,27 @@ import '../model/ride/ride.dart';
 ///   - The list of available rides
 ///
 class RidesService {
-  static List<Ride> allRides = fakeRides;
+  static List<Ride> availableRides = fakeRides; // TODO for now fake data
 
+  static List<Ride> filterByDeparture(Location departure) {
+    return availableRides
+        .where((availableRides) => availableRides.arrivalLocation == departure)
+        .toList();
+  }
 
+  static List<Ride> filterBySeatRequested(int seatRequested) {
+    return availableRides
+        .where(
+          (availableRides) => availableRides.availableSeats == seatRequested,
+        )
+        .toList();
+  }
+
+  static List<Ride> filterBy({Location? departure, int? seatRequested}) {
+    return availableRides
+        .where(
+          (availableRides) => availableRides.availableSeats == seatRequested,
+        )
+        .toList();
+  }
 }
